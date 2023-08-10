247ab9f1606e345bcbe1d13d35f79d85f419fde0
nicoCalde <nicogonzalezcalderon@gmail.com>
2023-08-10 18:37:34 -0300

    backwards

--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -7,8 +7,8 @@ import Maps from '../pages/Maps';
 const Routes = () => {
   return (
     <Switch> {/* Use Switch to ensure only one route is matched */}
-      <Route path='/mapai/mapas' component={Maps}/>
-      <Route path='/mapai' exact component={Home}/>
+      <Route path='/mapas' component={Maps}/>
+      <Route path='/' exact component={Home}/>
     </Switch>
   );
 }
